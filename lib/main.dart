711e4e6eec4d9dfa63225b6b348e62794dba5537
revenantonthemission <corpseonthemission@icloud.com>
2025-04-30 16:50:14 +0900

    Resolved version dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_gemini/flutter_gemini.dart';
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
+
+const apiKey = 'AIzaSyB9izMoFy3QCTvwY5nN_ZT25N6gsDonylw';
 
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
@@ -12,6 +14,8 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
+
+  Gemini.init(apiKey: apiKey);
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
